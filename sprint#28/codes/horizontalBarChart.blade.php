263352364f7821884698938b1478ec9bf9bfdf35
HIM ROUNGRIDH <roungridh.him@TURBOTECH.COM>
2022-11-11 08:22:16 +0700

    sprint#28

---
+++ sprint#28/codes/horizontalBarChart.blade.php
@@ -0,0 +1,91 @@
+@extends('master')
+@section('content')
+
+{{-- <div class="headingContainer">
+    <h1 class="headingText">  <i class="{{ $icon }} headingIcon"> </i> {{ $title }} </h1>
+</div> --}}
+
+<!-- Design Horizontal Bar Chart -->
+
+<!-- main -->
+<div class="mainCard">
+    <ul class="cardContainer grid-cols-1">
+        <li class="eachCard px-0 py-2 pr-2 mt-5 sm:mt-0 rounded-xl">
+            <div class="p-0 sm:p-2 md:p-5">
+                <canvas id="horizontal-bar" class="min-h-min w-full sm:min-h-[20rem]"></canvas>
+            </div>
+        </li>
+    </ul>
+</div>
+
+@endsection
+
+@section('internalScript')
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
+
+<script>
+
+    // Horizontal Bar Chart function
+    const horizontalBarChart = () => {
+            
+        // Horizontal Bar Chart config
+        const horizontalBarChartConfig = {
+            maintainAspectRatio: true,
+            indexAxis: "y",
+            elements : {
+                borderWidth: 2,
+            },
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: "top",
+                },
+                title: {
+                    display: false, // title display none by false
+                    text: 'Chart.js Horizontal Bar Chart'
+                }
+            }
+        };
+
+        // Horizontal Bar Chart data
+        const horizontalBarChartData = {
+            labels : ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            datasets : [
+                {
+                    label : "Dataset 1",
+                    backgroundColor: "rgba(248,113,113,0.5)",
+                    borderColor: "rgba(248,113,113,1)",
+                    borderWidth: 2,
+                    hoverBackgroundColor: "rgba(248,113,113,1)",
+                    hoverBorderColor: "rgba(248,113,113,1)",
+                    data: [31.854, 19.211, -45.489, 3.186, -10.074, -45.761, 39.333],
+                },
+                {
+                    label : "Dataset 2",
+                    backgroundColor: "rgba(14,165,233,0.5)",
+                    borderColor: "rgba(14,165,233,1)",
+                    borderWidth: 2,
+                    hoverBackgroundColor: "rgba(14,165,233,1)",
+                    hoverBorderColor: "rgba(14,165,233,1)",
+                    data: [-85.655, -70.108, 6.962, 59.134, -51.862, -27.262, 90.96],
+                    stack: "combined",
+                }
+            ],
+        };
+
+        const barChart = $("#horizontal-bar");
+
+        const horizontalBar = new Chart(barChart, {
+            type: "bar",
+            options: horizontalBarChartConfig,
+            data: horizontalBarChartData
+        });
+    }
+
+    // call function
+    horizontalBarChart();
+
+</script>
+
+@endsection
---
+++ sprint#28/codes/stackedLinearCategory.blade.php
@@ -0,0 +1,102 @@
+@extends('master')
+@section('content')
+
+{{-- <div class="headingContainer">
+    <h1 class="headingText">  <i class="{{ $icon }} headingIcon"> </i> {{ $title }} </h1>
+</div> --}}
+
+<!-- Design Stacked Linear / Category -->
+
+<!-- main -->
+<div class="mainCard">
+    <ul class="cardContainer grid-cols-1">
+        <li class="eachCard px-0 py-2 pr-2 mt-5 sm:mt-0 rounded-xl">
+            <div class="p-0 sm:p-2 md:p-5">
+                <canvas id="stacked-linear-category" class="min-h-min w-full sm:min-h-[20rem]"></canvas>
+            </div>
+        </li>
+    </ul>
+</div>
+
+@endsection
+
+@section('internalScript')
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
+
+<script>
+
+    // Stacked Linear / Category function
+    const stackedLinearCategory = () => {
+
+        // Stacked Linear / Category config
+        const stackedLinearCategoryConfig = {
+
+            responsive: true,
+            plugins: {
+                title: {
+                    display: false,
+                    text: "Stacked scales",
+                },
+            },
+            scales: {
+                y: {
+                    type: "linear",
+                    position: "left",
+                    stack: "demo",
+                    stackWeight: 2,
+                    grid: {
+                        borderColor: "rgba(248,113,113,1)",
+                    },
+                },
+                y2: {
+                    type: "category",
+                    labels: ["ON", "OFF"],
+                    offset: true,
+                    position: "left",
+                    stack: "demo",
+                    stackWeight: 1,
+                    grid: {
+                        borderColor: "rgba(14,165,233,1)",
+                    },
+                },
+            },
+        };
+
+        // Stacked Linear / Category data
+        const stackedLinearCategoryData = {
+            labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            datasets: [
+                {
+                    label: "Dataset 1",
+                    backgroundColor: "rgba(248,113,113,1)",
+                    borderColor: "rgba(248,113,113,1)",
+                    data: [10, 30, 50, 20, 25, 44, -10],
+                },
+                {
+                    label: "Dataset 2",
+                    backgroundColor: "rgba(14,165,233,1)",
+                    borderColor: "rgba(14,165,233,1)",
+                    data: ["ON", "ON", "OFF", "ON", "OFF", "OFF", "ON"],
+                    stepped: true,
+                    yAxisID: "y2"
+                },
+            ],
+        };
+
+        const barChart = $("#stacked-linear-category");
+
+        const stackedLinearCategoryChart = new Chart(barChart, {
+            type: "line",
+            options: stackedLinearCategoryConfig,
+            data: stackedLinearCategoryData
+        });
+
+    }
+
+    // call function
+    stackedLinearCategory();
+    
+</script>
+
+@endsection
